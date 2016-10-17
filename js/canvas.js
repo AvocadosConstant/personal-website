e8e3560b268b879a2f97303af185e3bd7e4bdbea
Tim Hung <thung1@binghamton.edu>
2016-10-17 18:15:47 -0400

    Launch new initial draft of redesign

--- js/canvas.js
+++ js/canvas.js
@@ -1,6 +1,38 @@
+function generateTriangles() {
+  console.log("Generating triangles");
+  var triangles = Delaunay.triangulate(vertices);
+
+  for(var i = triangles.length; i; ) {
+    ctx.beginPath();
+    var colVal = triColors[i];
+    --i; ctx.moveTo(vertices[triangles[i]][0], vertices[triangles[i]][1]);
+    --i; ctx.lineTo(vertices[triangles[i]][0], vertices[triangles[i]][1]);
+    --i; ctx.lineTo(vertices[triangles[i]][0], vertices[triangles[i]][1]);
+    ctx.closePath();
+    //ctx.stroke();
+    ctx.fillStyle = 
+      "rgba(" + 
+      colVal + "," +  
+      colVal + "," +  
+      colVal + ", 1)";
+    ctx.fill();
+  }
+}
+
+function jiggleVertices() {
+  console.log("Commence jiggling");
+  for(var i = 0; i < vertices.length; i++) {
+    var x = vertices[i][0] + randRange(-5, 5);
+    var y = vertices[i][1] + randRange(-5, 5);
+    //vertices[i] = [x, y];
+  }
+  generateTriangles();
+}
+
 function randRange(min, max) {
     return Math.floor(Math.random() * (max - min + 1)) + min;
 }
+
 var CANVAS = {
   id: "bgCanvas",
   width: Math.max(document.documentElement.clientWidth, window.innerWidth || 0), 
@@ -12,41 +44,24 @@ var CANVAS = {
 var canvas = document.getElementById(CANVAS.id);
 var ctx = canvas.getContext("2d");
 
-//  draw canvas
+// initialize canvas
 ctx.canvas.width  = CANVAS.width;
 ctx.canvas.height = CANVAS.height;
 ctx.fillStyle = CANVAS.color;
 ctx.fillRect(0, 0, CANVAS.width, CANVAS.height);
-
 ctx.lineWidth=1;
 ctx.strokeStyle="#dfdfdf";
 
-
 var vertices = new Array(120);
-
 for(var i = 0; i < vertices.length; i++) {
-    var x = 1.4 * Math.random() * canvas.width - 0.2 * canvas.width;
-    var y = 1.4 * Math.random() * canvas.height - 0.2 * canvas.height;
-    vertices[i] = [x, y];
+  var x = 1.4 * Math.random() * canvas.width - 0.2 * canvas.width;
+  var y = 1.4 * Math.random() * canvas.height - 0.2 * canvas.height;
+  vertices[i] = [x, y];
 }
 
-console.time("triangulate");
-var triangles = Delaunay.triangulate(vertices);
-console.timeEnd("triangulate");
-
-console.log("triangles.length = " + triangles.length);
-for(var i = triangles.length; i; ) {
-  ctx.beginPath();
-  --i; ctx.moveTo(vertices[triangles[i]][0], vertices[triangles[i]][1]);
-  --i; ctx.lineTo(vertices[triangles[i]][0], vertices[triangles[i]][1]);
-  --i; ctx.lineTo(vertices[triangles[i]][0], vertices[triangles[i]][1]);
-  ctx.closePath();
-  //ctx.stroke();
-  var colVal = randRange(230, 250);
-  ctx.fillStyle = 
-    "rgba(" + 
-    colVal + "," +  
-    colVal + "," +  
-    colVal + ", 1)";
-  ctx.fill();
+var triColors = new Array(Delaunay.triangulate(vertices).length);
+for(var i = 0; i < triColors.length; i++) {
+  triColors[i] = randRange(240, 250);
 }
+
+var timer = setInterval(jiggleVertices, 100);
